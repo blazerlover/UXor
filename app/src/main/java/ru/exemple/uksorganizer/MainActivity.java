8f89a1372312f71e349d0753543bf67af4137a2e
blazerlover <pashkovich1@yandex.ru>
2019-12-19 22:01:46 +0300

    option_menu_main added
    option_menu_main added
    menu have three items for now

--- app/src/main/java/ru/exemple/uksorganizer/MainActivity.java
+++ app/src/main/java/ru/exemple/uksorganizer/MainActivity.java
@@ -1,8 +1,11 @@
 package ru.exemple.uksorganizer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu_main, menu);
+        return true;
     }
 }
