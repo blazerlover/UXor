cdc45e401b61f1848e30f1ee7abf5a6164551f8f
blazerlover <pashkovich1@yandex.ru>
2019-12-22 19:07:35 +0300

    add FAB
    Add Fab

--- app/src/main/java/ru/exemple/uksorganizer/MainActivity.java
+++ app/src/main/java/ru/exemple/uksorganizer/MainActivity.java
@@ -6,6 +6,10 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "СОСАТЬ", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
     }
 
