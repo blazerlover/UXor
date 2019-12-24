0ca908de2d5b7561a2cf4dbe1ea407b6c1ffea01
Breakbeatman <iga1309@mail.ru>
2019-12-24 20:16:02 +0300

    Update_CP3_24.12.19

--- app/src/main/java/ru/exemple/uksorganizer/MainActivity.java
+++ app/src/main/java/ru/exemple/uksorganizer/MainActivity.java
@@ -1,8 +1,15 @@
 package ru.exemple.uksorganizer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "СОСАТЬ", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu_main, menu);
+        return true;
     }
 }
