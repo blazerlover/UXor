538c2ca4f30c19f246b41fcec2ef7facd29a6e02
blazerlover <pashkovich1@yandex.ru>
2020-01-15 20:49:14 +0300

    add Intent for Event_activiry

--- app/src/main/java/ru/exemple/uksorganizer/MainActivity.java
+++ app/src/main/java/ru/exemple/uksorganizer/MainActivity.java
@@ -3,30 +3,29 @@ package ru.exemple.uksorganizer;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "СОСАТЬ", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(this);
+    }
 
+    //Пока что слушатель только у одной кнопки, потом нужно будет пердусмотреть оператор switch/case или прикручивать к каждой кнопке отдельно
+    @Override
+    public void onClick(View view){
+        Intent intent = new Intent(this, EventActivity.class);
+        startActivity(intent);
     }
 
     @Override
